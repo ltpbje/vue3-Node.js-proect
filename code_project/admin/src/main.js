c8f176e9d69064782a39f1a6ea6a7b07c04909c1
乐天居易 <19836788252@163.com>
2024-09-07 21:12:21 +0800

    登录模块前端界面完成

--- code_project/admin/src/main.js
+++ code_project/admin/src/main.js
@@ -4,7 +4,16 @@ import router from "./router"
 import store from "./store"
 import ElementPlus from "element-plus"
 import "element-plus/dist/index.css"
+import Particles from "@tsparticles/vue3"
+import { loadFull } from "tsparticles" // if you are going to use `loadFull`, install the "tsparticles" package too.
+// import { loadSlim } from "tsparticles-slim" // if you are going to use `loadSlim`, install the "tsparticles-slim" package too.
 
 const app = createApp(App)
+app.use(Particles, {
+  init: async (engine) => {
+    await loadFull(engine) // you can load the full tsParticles library from "tsparticles" if you need it
+    // await loadSlim(engine) // or you can load the slim version from "tsparticles-slim" if don't need Shapes or Animations
+  },
+})
 app.use(ElementPlus)
 app.use(store).use(router).mount("#app")
