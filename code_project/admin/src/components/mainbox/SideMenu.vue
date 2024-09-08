43ba1893572922f16eaaccfe343764182262075e
乐天居易 <19836788252@163.com>
2024-09-08 10:48:27 +0800

    aside模块进行中

---
+++ code_project/admin/src/components/mainbox/SideMenu.vue
@@ -0,0 +1,66 @@
+<template>
+     <el-aside :width="$store.state.isCollapsed ? '64px' : '200px'">
+          <el-menu :collapse="$store.state.isCollapsed">
+               <el-menu-item index="/index">
+                    <el-icon>
+                         <HomeFilled />
+                    </el-icon>
+                    <span>首页</span>
+               </el-menu-item>
+               <el-menu-item index="/center">
+                    <el-icon>
+                         <Avatar />
+                    </el-icon>
+                    <span>个人中心</span>
+               </el-menu-item>
+
+               <el-sub-menu index="/user-manage">
+                    <template #title>
+                         <el-icon>
+                              <UserFilled />
+                         </el-icon>
+                         <span>用户管理</span>
+                    </template>
+
+                    <el-menu-item index="/user-manage/adduser">添加用户</el-menu-item>
+                    <el-menu-item index="/user-manage/userlist">用户列表</el-menu-item>
+
+               </el-sub-menu>
+               <el-sub-menu index="/news-manage">
+                    <template #title>
+                         <el-icon>
+                              <MessageBox />
+                         </el-icon>
+                         <span>新闻管理</span>
+                    </template>
+
+                    <el-menu-item index="/news-manage/newsadd">创建新闻</el-menu-item>
+                    <el-menu-item index="/news-manage/newslist">新闻列表</el-menu-item>
+
+               </el-sub-menu>
+               <el-sub-menu index="/product-manage">
+                    <template #title>
+                         <el-icon>
+                              <Reading />
+                         </el-icon>
+                         <span>产品管理</span>
+                    </template>
+
+                    <el-menu-item index="/product-manage/addproduct">添加产品</el-menu-item>
+                    <el-menu-item index="/product-manage/productlist">产品列表</el-menu-item>
+
+               </el-sub-menu>
+
+          </el-menu>
+     </el-aside>
+</template>
+
+<script setup>
+import { HomeFilled, Avatar, UserFilled, MessageBox, Reading, Pointer } from '@element-plus/icons-vue'
+</script>
+<style lang="scss" scoped>
+.el-aside {
+     height: 100vh;
+     transition: all 700ms;
+}
+</style>
--- code_project/admin/src/views/MainBox.vue
+++ code_project/admin/src/views/MainBox.vue
@@ -1,7 +1,20 @@
 <template>
 
-    <div>
-        MainBox
-        <router-view></router-view>
-    </div>
+  <div>
+    <el-container>
+      <SideMenu></SideMenu>
+      <el-container direction="vertical">
+        <TopHeader></TopHeader>
+        <el-main><router-view></router-view></el-main>
+      </el-container>
+    </el-container>
+
+  </div>
 </template>
+
+<script setup>
+import SideMenu from '@/components/mainbox/SideMenu.vue'
+import TopHeader from '@/components/mainbox/TopHeader.vue'
+</script>
+
+<style lang="scss" scoped></style>
