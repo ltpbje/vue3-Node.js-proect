0f5a6237331502ef7b7576fbcd0f572a9a431c30
乐天居易 <19836788252@163.com>
2024-09-13 21:39:48 +0800

    练习中

--- MongoDB学习/在node.js中操作MongoDB/code/server/routes/users.js
+++ MongoDB学习/在node.js中操作MongoDB/code/server/routes/users.js
@@ -13,15 +13,10 @@ router.post("/user/add",(req,res)=>{
   //插入数据库
   // 1. 创建一个模型(user,限制filed类型), 一一对应数据库的集合(users)
   // user.create user.find user.delete user.update
-  const {username,password,age} = req.body
-  UserModel.create({
-    username,password,age
-  }).then(data=>{
-    console.log(data)
-    res.send({
-      ok:1
-    })
+  res.send({
+    ok:1
   })
+
 })
 
 module.exports = router;
