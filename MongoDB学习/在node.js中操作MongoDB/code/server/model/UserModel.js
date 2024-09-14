a83972cce3c45366c8acbfe98af7456f42ff2c37
乐天居易 <19836788252@163.com>
2024-09-14 22:01:20 +0800

    MongoDB-nodejs操作-1

--- MongoDB学习/在node.js中操作MongoDB/code/server/model/UserModel.js
+++ MongoDB学习/在node.js中操作MongoDB/code/server/model/UserModel.js
@@ -3,13 +3,13 @@
  * @公众号: 大前端私房菜
  */
 const mongoose = require("mongoose")
-const Schema = mongoose.Schema
+
 const UserType = {
-    username:String,
-    password:String,
+    username: String,
+    password: String,
     age:Number
 }
 
-const UserModel = mongoose.model("user",new Schema(UserType))
+const UserModel = mongoose.model("user",mongoose.Schema(UserType))
 // 模型user 将会对应 users 集合, 
 module.exports = UserModel
