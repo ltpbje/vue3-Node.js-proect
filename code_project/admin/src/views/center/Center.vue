131ab0b8faa114f1951e83ecd03ff020c8202160
乐天居易 <19836788252@163.com>
2024-09-20 21:24:20 +0800

    server-Center接口

--- code_project/admin/src/views/center/Center.vue
+++ code_project/admin/src/views/center/Center.vue
@@ -22,6 +22,33 @@
                             <span>个人信息</span>
                         </div>
                     </template>
+
+                    <el-form ref="userFormRef" style="max-width: 600px" :model="userForm" :rules="userFormRules"
+                        label-width="auto" class="demo-ruleForm" status-icon>
+                        <el-form-item label="用户名" prop="username">
+                            <el-input v-model="userForm.username" />
+                        </el-form-item>
+                        <el-form-item label="性别" prop="gender">
+                            <el-select v-model="userForm.gender" placeholder="Select" style="width: 100%">
+                                <el-option v-for="item in options" :key="item.value" :label="item.label"
+                                    :value="item.value" />
+                            </el-select>
+                        </el-form-item>
+                        <el-form-item label="个人简介" prop="introduction">
+                            <el-input v-model="userForm.introduction" type="textarea" />
+                        </el-form-item>
+                        <el-form-item label="头像" prop="introduction">
+                            <el-upload class="avatar-uploader"
+                                action="https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15"
+                                :show-file-list="false" :on-success="handleAvatarSuccess"
+                                :before-upload="beforeAvatarUpload" :auto-upload="false">
+                                <img v-if="userForm.avatar" :src="userForm.avatar" class="avatar" />
+                                <el-icon v-else class="avatar-uploader-icon">
+                                    <Plus />
+                                </el-icon>
+                            </el-upload>
+                        </el-form-item>
+                    </el-form>
                 </el-card>
             </el-col>
 
@@ -32,10 +59,49 @@
 
 <script setup>
 import { useStore } from 'vuex'
-import { computed } from 'vue'
+import { computed, ref, reactive } from 'vue'
+import { Plus } from '@element-plus/icons-vue';
 const store = useStore()
 
 const avatarUrl = computed(() => store.state.userInfo.avatar ? store.state.userInfo.avatar : 'https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png')
+const { username, gender, introduction, avatar } = store.state.userInfo
+const userFormRef = ref()
+const userForm = reactive({
+    username,
+    gender,
+    introduction,
+    avatar
+})
+const userFormRules = reactive({
+    username: [
+        { required: true, message: '请输入用户名', trigger: 'blur' },
+    ],
+    gender: [
+        { required: true, message: '请输入性别', trigger: 'blur' },
+    ],
+    introduction: [
+        { required: true, message: '请输入简介', trigger: 'blur' },
+    ],
+    avatar: [
+        { required: true, message: '请上传头像', trigger: 'blur' },
+    ],
+})
+
+// 性别选项
+const options = [
+    {
+        label: '保密',
+        value: 0
+    },
+    {
+        label: '男',
+        value: 1
+    },
+    {
+        label: '女',
+        value: 2
+    },
+]
 </script>
 
 <style lang="scss" scoped>
@@ -45,5 +111,34 @@ const avatarUrl = computed(() => store.state.userInfo.avatar ? store.state.userI
     .box-card {
         text-align: center
     }
+}
+
+.avatar-uploader .avatar {
+    width: 178px;
+    height: 178px;
+    display: block;
+}
+</style>
+
+<style lang="scss" scoped>
+::v-deep .avatar-uploader .el-upload {
+    border: 1px dashed var(--el-border-color);
+    border-radius: 6px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+    transition: var(--el-transition-duration-fast);
+}
+
+::v-deep.avatar-uploader .el-upload:hover {
+    border-color: var(--el-color-primary);
+}
+
+::v-deep.el-icon.avatar-uploader-icon {
+    font-size: 28px;
+    color: #8c939d;
+    width: 178px;
+    height: 178px;
+    text-align: center;
 }
 </style>
